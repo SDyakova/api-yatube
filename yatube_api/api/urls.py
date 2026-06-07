33e511bae5992cc9ab4dfd7190037c954fc011c6
Svetlana Dyakova <dyakova030@gmail.com>
2026-06-07 09:28:33 +0300

    Переименовать роутер в router_v1 и добавить basename для всех роутеров

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,14 +4,14 @@ from rest_framework.routers import SimpleRouter
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-router = SimpleRouter()
-router.register("posts", PostViewSet)
-router.register("groups", GroupViewSet)
-router.register(
+router_v1 = SimpleRouter()
+router_v1.register("posts", PostViewSet, basename="post")
+router_v1.register("groups", GroupViewSet, basename="group")
+router_v1.register(
     r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comment"
 )
 
 urlpatterns = [
     path("api-token-auth/", token_views.obtain_auth_token),
-    path("", include(router.urls)),
+    path("", include(router_v1.urls)),
 ]
