03d50f65d98330e3e111cd0715f5fd202cd3469b
Svetlana Dyakova <dyakova030@gmail.com>
2026-06-07 09:25:23 +0300

    Убрать лишние переопределения полей, использовать read_only_fields

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -14,26 +14,16 @@ class GroupSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     """Сериализатор для постов."""
 
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field="username"
-    )
-    group = serializers.PrimaryKeyRelatedField(
-        queryset=Group.objects.all(), required=False, allow_null=True
-    )
-
     class Meta:
         model = Post
         fields = ("id", "text", "author", "image", "pub_date", "group")
+        read_only_fields = ("author",)
 
 
 class CommentSerializer(serializers.ModelSerializer):
     """Сериализатор для комментариев."""
 
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field="username"
-    )
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = Comment
         fields = ("id", "text", "author", "post", "created")
+        read_only_fields = ("author", "post")
