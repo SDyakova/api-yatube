96fb769464259bce1fa45295acceb0d4b2db138c
Svetlana Dyakova <dyakova030@gmail.com>
2026-06-07 10:00:12 +0300

    Улучшить модели posts

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,43 +3,74 @@ from django.db import models
 
 User = get_user_model()
 
+MAX_STR_LENGTH = 30
+
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="Название")
+    slug = models.SlugField(unique=True, verbose_name="Слаг")
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        default_related_name = "groups"
 
     def __str__(self):
-        return self.title
+        return self.title[:MAX_STR_LENGTH]
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name="Автор",
     )
-    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    image = models.ImageField(
+        upload_to="posts/",
+        null=True,
+        blank=True,
+        verbose_name="Изображение",
+    )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         related_name="posts",
         blank=True,
         null=True,
+        verbose_name="Группа",
     )
 
+    class Meta:
+        default_related_name = "posts"
+
     def __str__(self):
-        return self.text
+        return self.text[:MAX_STR_LENGTH]
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Автор",
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Пост",
     )
-    text = models.TextField()
+    text = models.TextField(verbose_name="Текст")
     created = models.DateTimeField(
-        "Дата добавления", auto_now_add=True, db_index=True
+        "Дата добавления",
+        auto_now_add=True,
+        db_index=True,
     )
+
+    class Meta:
+        default_related_name = "comments"
+
+    def __str__(self):
+        return f"{self.author.username[:15]}: " f"{self.text[:MAX_STR_LENGTH]}"
